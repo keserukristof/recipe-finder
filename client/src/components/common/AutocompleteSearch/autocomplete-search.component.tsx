4a4cf7e2ae35e0cf7ae47ae7b7bed81b013d94f5
Kristof Keseru <Kristof_Keseru@epam.com>
2023-08-02 17:57:21 +0200

    client: started the implementation of the autocomplete search

---
+++ client/src/components/common/AutocompleteSearch/autocomplete-search.component.tsx
@@ -0,0 +1,22 @@
+import { FunctionComponent } from "react";
+
+import { Autocomplete, TextField } from '@mui/material';
+
+interface AutocompleteSearchProps {
+  options: string[];
+  label: string;
+  onValueChange: (newValue: string | null) => void;
+}
+
+export const AutocompleteSearch: FunctionComponent<AutocompleteSearchProps> = ({ options, label, onValueChange }) => {
+  return (
+    <Autocomplete
+      disablePortal
+      id="autocomplete-search"
+      options={options}
+      sx={{ width: 300 }}
+      onChange={(event, newValue) => onValueChange(newValue)}
+      renderInput={(params) => <TextField {...params} label={label} />}
+    />
+  );
+}
