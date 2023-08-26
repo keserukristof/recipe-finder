5ffb349e9e095bec80d94be227f32ef8083e06db
Kristof Keseru <Kristof_Keseru@epam.com>
2023-08-26 10:28:39 +0200

    moved styles to separate styled component

---
+++ client/src/styled-components/recipe-additional-information-box.component.tsx
@@ -0,0 +1,11 @@
+import { styled } from '@mui/system';
+import { Box, BoxProps } from '@mui/material';
+
+const StyledRecipeAdditionalInformationBox = styled(Box)<BoxProps>({
+    display: 'flex',
+    alignItems: "center",
+    marginTop: "1rem",
+    marginBottom: "1rem",
+});
+
+export default StyledRecipeAdditionalInformationBox;
